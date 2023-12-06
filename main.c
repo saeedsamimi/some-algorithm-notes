7265afaed35a397b3f29675b75ba4bd2cbcd7eed
saeedsamimi <doob.fooy@gmail.com>
2023-12-06 22:06:23 +0330

    set something

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+int DivisorsCount(int n) {
+  int k = 0;
+  while (!(n % 2)) {
+    n /= 2;
+    k++;
+  }
+  int p = (k + 1);
+  for (int i = 3; n > 1; i += 2) {
+    k = 0;
+    while (!(n % i)) {
+      n /= i;
+      k++;
+    }
+    p *= (k + 1);
+  }
+  return p;
+}
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  int n;
+  scanf("%d", &n);
+  printf("%d", DivisorsCount(n));
+  return 0;
 }
